b30d0e53bb70fad5d58dc260918fc0ed296f2e75
Jamie Reardon <decor1878@gmail.com>
2019-09-12 14:29:35 +0100

    Customize jQuery plugin options and insert project main js file

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Customize Lightbox2 plugin options
+lightbox.option({
+    'alwaysShowNavOnTouchDevices': true,
+    'fadeDuration': 600,
+    'imageFadeDuration': 1000,
+    'resizeDuration': 600,
+    'showImageNumberLabel': false
+  });
